50037f314704a968121d2bbbb3fa7af7acb18a7f
Jplatea <103559122+Jplatea@users.noreply.github.com>
2026-08-19 11:00:14 +0200

    Fix duplicate Redes component in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -27,10 +27,10 @@ const Index = () => {
       <Vestidor />
       <Poemas />
       <InstagramFeed />
+      <Redes />
       <Visitas />
       <Contacto />
       <SiteFooter />
-            <Redes />
     </div>
   );
 };
